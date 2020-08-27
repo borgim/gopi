94db16b7e3fef95949a5e5b5b83e9aa8ee6e8548
Pedro Borges <BorgesPedro@users.noreply.github.com>
2020-08-27 00:55:53 -0300

    Update gopi.php

--- gopi.php
+++ gopi.php
@@ -18,27 +18,27 @@ if (count($argv) < 2) {
   ██║  ███╗██║   ██║██████╔╝██║
   ██║   ██║██║   ██║██╔═══╝ ██║
   ╚██████╔╝╚██████╔╝██║     ██║
-   ╚═════╝  ╚═════╝ ╚═╝     ╚═╝\e[0m \e[1;34mCrypt 1.0\e[0m\n  Usage:\n  -> php gopi.php crypt\n  -> php gopi.php decrypt";
+   ╚═════╝  ╚═════╝ ╚═╝     ╚═╝\e[0m \e[1;34mCrypt 1.0\e[0m". PHP_EOL."  Usage:".PHP_EOL. "  -> php gopi.php crypt".PHP_EOL."  -> php gopi.php decrypt";
 }else{
 	switch ($argv[1]) {
 		case 'crypt':
-			echo "\e[1;35mType what you want to encrypt\e[0m\n";
+			echo "\e[1;35mType what you want to encrypt\e[0m".PHP_EOL;
 			$cryptcontent = readline("> ");
-			echo $cryptcontent . "\n";
-			echo "\e[1;35mType key for encryption\e[0m\n";
+			echo $cryptcontent . PHP_EOL;
+			echo "\e[1;35mType key for encryption\e[0m". PHP_EOL;
 			$cryptkey = readline("> ");
 			echo $cryptkey;
 			break;
 		case 'decrypt':
-			echo "\e[1;35mType file you want to dencrypt\e[0m\n";
+			echo "\e[1;35mType file you want to dencrypt\e[0m".PHP_EOL;
 			$cryptcontent = readline("> ");
-			echo $cryptcontent . "\n";
-			echo "\e[1;35mType key for decryption\e[0m\n";
+			echo $cryptcontent . PHP_EOL;
+			echo "\e[1;35mType key for decryption\e[0m".PHP_EOL;
 			$cryptkey = readline("> ");
 			echo $cryptkey;
 			break;
 		default:
-			echo "  Usage:\n  -> php gopi.php crypt\n  -> php gopi.php decrypt";
+			echo "  Usage:".PHP_EOL."  -> php gopi.php crypt".PHP_EOL."  -> php gopi.php decrypt";
 			break;
 	};
 }
