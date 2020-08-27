0a3d6a9c6af2eb85717685e8196b8333351a1e4a
Pedro Borges <BorgesPedro@users.noreply.github.com>
2020-08-26 22:34:50 -0300

    Update file.class.php

--- file.class.php
+++ file.class.php
@@ -3,25 +3,36 @@
 class File
 {
     const EXTENSION = ['.jpg', '.png', '.jpeg', '.txt', '.pdf']; //constante contendo as extensoes validas
+    const NCORRECT = "Error. File does not exists or is empty";
     public $file; //atributo que salva o arquivo aberto
     public $filecontent; //atributo para salvar o conteudo do arquivo
    
     //abrir
     public function open($file)//recebe um argumento com o nome do arquivo
     {
-        if(in_array(strstr($file, "."), self::EXTENSION)){//verifica se a extensao desse arquivo é valida na constante EXTENSION
-            $this->file = fopen($file, "a+");//caso seja verdadeiro abre o arquivo salvando ele no atributo $file
+        if(file_exists($file)){//verifica se o arquivo existe no diretorio
+            if(in_array(strstr($file, "."), self::EXTENSION)){//verifica se a extensao desse arquivo é valida na constante EXTENSION
+                $this->file = fopen($file, "a+");//caso seja verdadeiro abre o arquivo salvando ele no atributo $file
+            }
+        }else{
+            return self::NCORRECT;
+            die();
         }
     }
     
     //ler
     public function read($file)//recebe um argumento com o nome do arquivo a ser lido
     {  
-        if(file_get_contents($file) == ''){//verifica se o conteudo do arquivo for vazio
-            return "Error, file is empty";//se for vazio, retorna erro
+        if(file_exists($file)){
+            if(!file_get_contents($file)){//verifica se o conteudo do arquivo for vazio
+                return self::NCORRECT;//se for vazio, retorna erro
+                die();
+            }else{
+                $this->filecontent = file_get_contents($file);//se não for vazio, pega o conteudo e coloca dentro do atributo $filecontent
+                return $this->filecontent;//retorna o que foi salvo em $filecontent
+            }
         }else{
-            $this->filecontent = file_get_contents($file);//se não for vazio, pega o conteudo e coloca dentro do atributo $filecontent
-            return $this->filecontent;//retorna o que foi salvo em $filecontent
+            return self::NCORRECT;
         }
     }
   
