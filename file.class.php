7fb5c94e4b96a41d11160d318a9308bf6221707f
Pedro Borges <BorgesPedro@users.noreply.github.com>
2020-09-03 00:02:19 -0300

    Update file.class.php

--- file.class.php
+++ file.class.php
@@ -1,53 +1,75 @@
 <?php
+/* 
+classe arquivo
+	o que precisa para funcionar? 
+        Arquivo
+        tipo de abertura
+	quais serao suas funcionalidades?
+		ler o arquivo
+			o que preciso para ler o arquivo?
+                apenas a funcao nativa do PHP file_get_contents
+                //$a1->setArquivo("nome_arquivo")
+                //$a1->ler();
+		escrever no arquivo
+            o que preciso para escrever no arquivo?
+                perguntar o tipo da abertura a+ ou w+
+                abrir o arquivo com fopen
+                depois fechar o arquivo
+                //$a1->setArquivo("nome_arquivo")
+                //$a1->setTipo(1)
+                //$a1->escrever("conteudo")
+*/
+class Arquivo{
+    private $arquivo;
+    private $tipo;
+    private $arquivo_aberto;
+    //funcionalidades
 
-class File
-{
-    const EXTENSION = ['.jpg', '.png', '.jpeg', '.txt', '.pdf']; //constante contendo as extensoes validas
-    const NCORRECT = "Error. File does not exists or is empty";
-    public $file; //atributo que salva o arquivo aberto
-    public $filecontent; //atributo para salvar o conteudo do arquivo
-   
-    //abrir
-    public function open($file)//recebe um argumento com o nome do arquivo
+    //pegar nome do arquivo
+    public function setArquivo($arquivo)
     {
-        if(file_exists($file)){//verifica se o arquivo existe no diretorio
-            if(in_array(strstr($file, "."), self::EXTENSION)){//verifica se a extensao desse arquivo é valida na constante EXTENSION
-                $this->file = fopen($file, "a+");//caso seja verdadeiro abre o arquivo salvando ele no atributo $file
-            }
+        $this->arquivo = $arquivo;
+
+    }
+    private function getArquivo()
+    {
+        return $this->arquivo;
+
+    }
+    //settar o tipo da abertura
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+    }
+    private function getTipo()
+    {
+        return $this->tipo;
+    }
+    //ler arquivo
+    public function ler()
+    {
+        return file_get_contents($this->getArquivo());
+    }
+
+    public function escrever($conteudo)
+    {
+        if($this->getTipo() == 1){
+            $modo = 'a+';
+        }elseif ($this->getTipo() == 2) {
+            $modo = 'w+';
         }else{
-            return self::NCORRECT;
             die();
         }
-    }
-    
-    //ler
-    public function read($file)//recebe um argumento com o nome do arquivo a ser lido
-    {  
-        if(file_exists($file)){
-            if(!file_get_contents($file)){//verifica se o conteudo do arquivo for vazio
-                return self::NCORRECT;//se for vazio, retorna erro
-                die();
-            }else{
-                $this->filecontent = file_get_contents($file);//se não for vazio, pega o conteudo e coloca dentro do atributo $filecontent
-                return $this->filecontent;//retorna o que foi salvo em $filecontent
-            }
-        }else{
-            return self::NCORRECT;
-        }
-    }
-  
-    //escrever
-    public function write($cryptcontent)//recebe um argumento contendo o conteudo a ser encriptado
-    {
-           ftruncate($this->file, 0);//apaga o conteudo do arquivo aberto
-           $this->filecontent = null;//filecontent passa a receber null
-           if(fwrite($this->file, $cryptcontent)){//verifica se foi possivel escrever o conteudo dentro do arquivo
-               return true;//retorna verdadeiro se foi possivel salvar o conteudo
-           }else{
-               return false;//retorna falso se não foi possível salvar o conteudo
-           }
-    }
-    public function close(){//metedo para fechar o arquivo aberto
-        fclose($this->file);
+       $this->arquivo_aberto = fopen($this->getArquivo(), $modo);
+       fwrite($this->arquivo_aberto, $conteudo."\n");
+       fclose($this->arquivo_aberto);
     }
 }
+//documentacao
+    //definindo arquivo
+        //->setArquivo('nome_arquivo');
+    //ler arquivo
+        //->ler(getArquivo());
+    //editar arquivo
+        //->setTipo(1 or 2)
+        //->escrever('conteudo');
